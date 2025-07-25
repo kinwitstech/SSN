6a3f4c95158c4905cc498087de173984b50898a9
SaiPraneeth <saipraneeth.kinwits@gmail.com>
2025-07-25 19:04:12 +0530

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 
 import App from "@/App.jsx";
 import "@/lib/i18n.js";
-import "./styles/index.css";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
